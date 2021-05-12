c4b67215e47c0abba0401e631cad1a4e7b35d5f6
bsgribtmfu <bsgribtmfu@mailfence.com>
2021-05-13 00:45:11 +0200

    fixed bugs in the css, added js [no refactoring]

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,8 @@
 const phonesValue = document.querySelectorAll('.form__input-phone');
+const phoneValue = document.querySelector('.form__input-phone');
+const forms = document.querySelectorAll('.form');
 const modalClose = document.querySelector('.modal__close');
+const modalNotificationClose = document.querySelector('.modal-notification__close');
 const modal = document.querySelector('.modal');
 const buttonCountries = document.querySelector('.countries__button');
 const buttonCard = document.querySelector('.card__button');
@@ -7,10 +10,12 @@ const buttonsCards = document.querySelectorAll('.card__button');
 const headerLogo = document.querySelector('.header__logo');
 const headerToggle = document.querySelector('.header__toggle');
 const headerNavigation = document.querySelector('.header__navigation');
+const modalNotification = document.querySelector('.modal-notification');
+const placesLinks = document.querySelectorAll('.places__link');
+const tours = document.querySelector('.tours__item');
+const navItems = document.querySelectorAll('.tours__navigation-link');
+const countries = document.querySelectorAll('.countries__item');
 
-// const changeForm = (value) => {
-//     modal.style.display = value;
-// }
 
 buttonsCards.forEach(element => {
     element.addEventListener('click', function() {
@@ -30,28 +35,91 @@ phonesValue.forEach(element => {
             element.placeholder = element.getAttribute('placeholder');
         }
     })
-    element.addEventListener('keyup', function() {
+    element.addEventListener('keyup', function(e) {
         element.value = element.value.replace(/[а-яА-Яa-zA-Z]/g, '');
+
+        if (element.value.length < 3) {
+            element.value = '+7 ';
+        }
     });
+})
+
+forms.forEach(element => {
+    element.addEventListener('submit', function(e) {
+        e.preventDefault();
+        if (phoneValue === 10) {
+            console.log('Данные не верны!');
+        }
+        else {
+            element.reset();
+            modalNotification.style.display = "block";
+        }
+    })
 })
 
 modalClose.addEventListener("click", function() {
     modal.style.display = "none";
 })
 
-buttonCountries.addEventListener("click", function() {
-    modal.style.display = "block";
+modalNotificationClose.addEventListener("click", function() {
+    modalNotification.style.display = "none";
 })
 
 buttonCountries.addEventListener("click", function() {
     modal.style.display = "block";
 })
 
-const checkToggle = () => {
-    if (headerToggle.style.display === '') {}
-}
+buttonCountries.addEventListener("click", function() {
+    modal.style.display = "block";
+})
 
 headerToggle.addEventListener("click", function() {
-    headerLogo.classList.add('header__logo--hidden');
-    headerNavigation.style.display = "block";
+    if (headerToggle.classList.contains('header__toggle--closed')) {
+        headerToggle.classList.remove('header__toggle--closed');
+        headerToggle.classList.add('header__toggle--opened');
+        headerLogo.style.display = "none";
+        headerNavigation.style.display = "block";
+    }
+    else if (headerToggle.classList.contains('header__toggle--opened')) {
+        headerToggle.classList.remove('header__toggle--opened');
+        headerLogo.classList.add('header__logo--show');
+        headerToggle.classList.add('header__toggle--closed');
+        headerLogo.style.display = "block";
+        headerNavigation.style.display = "none";
+    }
 })
+
+const events = (element) => {
+    element.addEventListener("click", function(e) {
+        e.preventDefault();
+        let country = element.dataset.country;
+
+        navItems.forEach(item => {
+            if (item.classList.contains('tours__navigation-link--active')) {
+                item.classList.remove('tours__navigation-link--active')
+            }
+
+            if (item.dataset.country === country) {
+                item.classList.add('tours__navigation-link--active');
+            }
+        });
+
+        countries.forEach(item => {
+            if (item.classList.contains('countries__item--active')) {
+                item.classList.remove('countries__item--active');
+            }
+
+            if (item.dataset.country === country) {
+                item.classList.add('countries__item--active');
+            }
+        });
+    });
+}
+
+placesLinks.forEach(element => {
+    events(element);
+});
+
+navItems.forEach(element => {
+    events(element);
+});
